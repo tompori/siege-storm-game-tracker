bf9f80087c758c40fc876cd234a770577cad91d0
tompori <nuorivaara@gmail.com>
2019-04-28 11:34:46 +0300

    React router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import styles from "./App.module.css";
 
 export default class App extends React.Component<{}, {}> {
   public render() {
-    return <div className={styles.App}>Hello again!</div>;
+    return (
+      <Router>
+        <div className={styles.App}>{this._renderHeader}</div>
+        <Route exact path="/" component={this._renderListGames} />
+        <Route path="/add" component={this._renderAddGame} />
+      </Router>
+    );
   }
+
+  private _renderHeader = (
+    <div>
+      <Link to="/">List games</Link>
+      <Link to="/add">Add a game</Link>
+    </div>
+  );
+
+  private _renderListGames = () => <div>List of games</div>;
+
+  private _renderAddGame = () => <div>Add a game</div>;
 }
