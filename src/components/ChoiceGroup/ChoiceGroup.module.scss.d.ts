35fa7b3d642b90230bf1e2f6fcedf3be4621b920
tompori <nuorivaara@gmail.com>
2019-05-31 23:25:47 +0300

    Fixed ChoiceGroup styles

--- src/components/ChoiceGroup/ChoiceGroup.module.scss.d.ts
+++ src/components/ChoiceGroup/ChoiceGroup.module.scss.d.ts
@@ -1,6 +1,7 @@
 declare const styles: {
   readonly "ChoiceGroup": string;
   readonly "choiceGroupItem": string;
+  readonly "isSelected": string;
   readonly "choiceTitle": string;
 };
 export = styles;
--- src/components/ChoiceGroup/ChoiceGroup.tsx
+++ src/components/ChoiceGroup/ChoiceGroup.tsx
@@ -14,14 +14,21 @@ interface IChoiceGroupProps {
 }
 
 const ChoiceGroup: React.FunctionComponent<IChoiceGroupProps> = props => {
+  const isSelected = value => props.stateData[props.valueKey] === value;
+
   return (
     <div className={styles.ChoiceGroup}>
       {props.choices.map((choice, index) => (
-        <label className={styles.choiceGroupItem} key={index}>
+        <label
+          className={`${styles.choiceGroupItem} ${
+            isSelected(choice.value) ? styles.isSelected : ""
+          }`}
+          key={index}
+        >
           <input
             type="radio"
             value={choice.value}
-            checked={props.stateData[props.valueKey] === choice.value}
+            checked={isSelected(choice.value)}
             onChange={event => props.handleChange(props.valueKey, choice.value)}
           />
           <span className={styles.choiceTitle}>{choice.title}</span>
